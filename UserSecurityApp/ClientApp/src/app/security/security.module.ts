58dc454f63e52746ba63e9fccfabf205489accbd
subrato2393 <subrato2393@gmail.com>
2023-02-24 23:37:26 +0600

    adding initial

---
+++ UserSecurityApp/ClientApp/src/app/security/security.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SecurityRoutingModule } from './security-routing.module';
+//import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+   // RouterModule,
+    SecurityRoutingModule
+  ]
+})
+export class SecurityModule { }
